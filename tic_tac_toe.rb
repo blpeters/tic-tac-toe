9ca8860a124c026bd955db97567c657733b3b79d
blpeters <brett.peters@utexas.edu>
2022-09-22 17:43:34 -0500

    board display updates

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,30 +1,42 @@
 # frozen_string_literal: true
 
 # Tic Tac Toe Board display to console, including completed moves.
-# This class only displays and updates the display of the board
-# This class does not initiate any changes or declare any winners!
-class Board
+class TicTacToe
+  attr_accessor :square_values
+  
   # TODO: when a new instance of Board is created, a new board needs to be
   # displayed and a new board array with 9 slots needs to be created.
+  def initialize
+    @square_values = ['X', 2, 3, 4, 5, 6, 7, 8, 9]
+  end
+
   def display
-    puts <<~BOARD.strip
-      1 | 2 | 3
+    # TODO: look further into Ruby HEREDOCS
+    puts <<~BOARD
+      #{square_values[0]} | #{square_values[1]} | #{square_values[2]}
       ----------
-      4 | 5 | 6
+      #{square_values[3]} | #{square_values[4]} | #{square_values[5]}
       ----------
-      7 | 8 | 9
+      #{square_values[6]} | #{square_values[7]} | #{square_values[8]}\n\n
     BOARD
   end
 
-  # An update method will take in parameters for which player (or symbol) and
-  # what location (square) on the board needs updating and update that location
+  # The update method will take in parameters for the current player's symbol and
+  # what location (square) was chosen by the player and update that location
   # in the array.
   def update(symbol, location)
-    puts symbol
-    puts location
+    square_values[location - 1] = symbol
+    display
   end
 end
 
-tic_tac_toe_board = Board.new
-tic_tac_toe_board.display
-tic_tac_toe_board.update('X', '2')
+class Player
+  def initialize(number)
+    @player_num = number
+  end
+end
+
+player1 = Player.new(1)
+player2 = Player.new(2)
+tic_tac_toe = TicTacToe.new
+tic_tac_toe.update('X', 2)
